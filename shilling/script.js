d43dfbfb37a34971308034e83cb6ba03582d36a7
belvonefelpato <belvonefelpato@protonmail.com>
2022-07-12 10:10:49 +0900

    Update script.js

--- shilling/script.js
+++ shilling/script.js
@@ -25,7 +25,7 @@ function retrieveText() {
             }
 
             // Send a request
-            xmlhttp.open("GET", "https://belvonefelpato.github.io/shilling/shilltext.txt", true);
+            xmlhttp.open("GET", "https://docs.google.com/document/d/1IzBp8nhbGh6XxH4epfW6G1Wh5mDTPwsRAauCBYqaDD0/export?format=txt", true);
             xmlhttp.send();
   
   
